5a80095d0a7a9f20f6234cbf2266e0c000d0a475
Nazar Khmel <nazkhmel@gmail.com>
2025-03-08 11:48:11 +0100

    fff

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _ProductsPageState extends State<ProductsPage> {
 
   Future<void> fetchProducts() async {
     final response = await http.get(Uri.parse(
-        'https://fac6-46-230-134-254.ngrok-free.app/products'));
+        'https://90b2-2001-4650-24fd-0-94b3-43f5-ff9d-fbd7.ngrok-free.app/products'));
 
     if (response.statusCode == 200) {
       List data = json.decode(response.body);
@@ -49,7 +49,7 @@ class _ProductsPageState extends State<ProductsPage> {
 
   Future<void> placeOrder(int productId) async {
     final response = await http.post(
-      Uri.parse('https://fac6-46-230-134-254.ngrok-free.app/orders'),
+      Uri.parse('https://90b2-2001-4650-24fd-0-94b3-43f5-ff9d-fbd7.ngrok-free.app/orders'),
       headers: {"Content-Type": "application/json"},
       body: json.encode({
         "room_id": widget.roomId, 
--- lib/orders_page.dart
+++ lib/orders_page.dart
@@ -22,7 +22,7 @@ class _OrdersPageState extends State<OrdersPage> {
 
   Future<void> fetchOrders() async {
     final response = await http.get(Uri.parse(
-        'https://fac6-46-230-134-254.ngrok-free.app/orders/${widget.roomId}')); // Подставляем roomId в URL
+        'https://90b2-2001-4650-24fd-0-94b3-43f5-ff9d-fbd7.ngrok-free.app/orders/${widget.roomId}')); // Подставляем roomId в URL
 
     if (response.statusCode == 200) {
       setState(() {
@@ -35,7 +35,7 @@ class _OrdersPageState extends State<OrdersPage> {
 
   Future<void> confirmOrder(int orderId) async {
     final response = await http.post(
-      Uri.parse('https://fac6-46-230-134-254.ngrok-free.app/confirm/$orderId'),
+      Uri.parse('https://90b2-2001-4650-24fd-0-94b3-43f5-ff9d-fbd7.ngrok-free.app/confirm/$orderId'),
     );
 
     if (response.statusCode == 200) {
